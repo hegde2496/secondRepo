81d72f90e43acb5109ed8fd2cb626fefac659c23
Prashanth S Hegde <Prashanth S Hegde@DESKTOP-IB68LVM>
2020-09-29 15:01:22 +0530

    testcommit

---
+++ Maven-Demo/src/main/java/com/tcs/App.java
@@ -0,0 +1,14 @@
+package com.tcs;
+
+public class App {
+	
+public String Sample(){
+	return "Sample" ;
+}
+
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
